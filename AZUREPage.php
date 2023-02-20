675ff8ef8f688aed87bd5a083f1d61a652ff11ff
MohammedEl2000 <71752347+MohammedEl2000@users.noreply.github.com>
2023-02-20 23:52:18 +0000

    Update AZUREPage.php

--- AZUREPage.php
+++ AZUREPage.php
@@ -1,17 +1,18 @@
+<?php include "../html/azuredbinfo.inc"; ?>
 <html>
 <body>
 <h1>Sample page</h1>
 <?php
 
   /* Connect to MySQL and select the database. */
-  $connection = mysqli_connect("terraformserver.mysql.database.azure.com", "mysqladmin", "Password123");
+  $connection = mysqli_connect((DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
   if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-  $database = mysqli_select_db($connection, "exampledb");
+  $database = mysqli_select_db($connection, DB_DATABASE);
 
   /* Ensure that the EMPLOYEES table exists. */
-  VerifyEmployeesTable($connection, "exampledb");
+  VerifyEmployeesTable($connection, DB_DATABASE);
 
   /* If input fields are populated, add a row to the EMPLOYEES table. */
   $employee_name = htmlentities($_POST['NAME']);
